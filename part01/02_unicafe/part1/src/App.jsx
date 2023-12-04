c0af0e11e5f0f67272ba59d50637e83ee8f03a20
ROGSTRIXG15\apere <abelperdan@gmail.com>
2023-12-04 20:00:08 +0100

    Exercise 1.7

--- part01/02_unicafe/part1/src/App.jsx
+++ part01/02_unicafe/part1/src/App.jsx
@@ -6,6 +6,16 @@ import Statistic from "./Statistic"
 import './App.css'
 
 const App = () => {
+
+  const getDivisor = () => {
+    let divisor = getAll();
+    return divisor = divisor > 0 ? divisor : 1;
+  }
+
+  const getAll = () => good + neutral + bad;
+  const getAverage = () => (good * 1 + neutral * 0 + bad * -1) / getDivisor();
+  const getPositiveAverage = () => good / getDivisor();
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -35,6 +45,11 @@ const App = () => {
         <Statistic text="Good" val={good} />
         <Statistic text="Neutral" val={neutral} />
         <Statistic text="Bad" val={bad} />
+
+        <hr />
+        <Statistic text="Total" val={getAll()} />
+        <Statistic text="Average" val={getAverage()} />
+        <Statistic text="Posit. avg" val={getPositiveAverage()} />
 
       </div>
 
