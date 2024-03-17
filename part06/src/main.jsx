bd72bb8cfefdbadd4e378d667a96f831e2f6410f
ab <abelperdan@gmail.com>
2024-03-17 12:05:33 +0100

    Exercise 6.2

--- part06/src/main.jsx
+++ part06/src/main.jsx
@@ -15,10 +15,10 @@ const App = () => {
 
   return (
     <div>
-      <button onClick={() => updateStore('GOOD')}>good</button>
-      <button onClick={() => updateStore('OK')}>ok</button>
-      <button onClick={() => updateStore('BAD')}>bad</button>
-      <button onClick={() => updateStore('ZERO')}>reset stats</button>
+      <button onClick={() => updateStore('GOOD')}>Good</button>
+      <button onClick={() => updateStore('OK')}>Ok</button>
+      <button onClick={() => updateStore('BAD')}>Bad</button>
+      <button onClick={() => updateStore('ZERO')}>Reset stats</button>
       <div>good: {store.getState().good}</div>
       <div>ok: {store.getState().ok}</div>
       <div>bad: {store.getState().bad}</div>
