95d7666915a91d086b4c9465cb0085464a9b1960
ab <abelperdan@gmail.com>
2024-03-18 21:21:25 +0100

    Exercises 6.6-6.8

--- part06_2/src/App.jsx
+++ part06_2/src/App.jsx
@@ -1,32 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { increaseVotes } from './reducers/anecdoteReducer.js'
-import NewAnecdote from './components/newAnecdote.jsx'
+
+import NewAnecdote from './components/NewAnecdote.jsx'
+import AnecdoteList from './components/AnecdoteList.jsx'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(increaseVotes(id))
-    console.log('vote', id)
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <NewAnecdote />
-
     </div>
   )
 }
--- part06_2/src/components/newAnecdote.jsx
+++ part06_2/src/components/newAnecdote.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux'
-import { createAnecdote } from '../reducers/anecdoteReducer'
+import { createAnecdote } from '../reducers/anecdoteReducer.js'
 
 const NewAnecdote = () => {
     const dispatch = useDispatch()
